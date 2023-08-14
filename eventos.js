9167818f9ea9c3aab7e6f52441416a49169fc897
matilopez31 <matilopez3110@gmail.com>
2023-08-13 21:39:57 -0300

    Boton del div
    boton con respuesta desde js

---
+++ eventos.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function()
+{
+    const divboton = document.querySelector('.boton');
+    divboton.addEventListener('click', function()
+    {
+        alert('Hola! Soy el div');
+    });
+});
